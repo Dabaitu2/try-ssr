b9ba0c54cfd6b5d827437de438cf49579adbdec4
tomokokawase <331555765@qq.com>
2021-01-04 20:25:04 +0800

    feat(all): add commitzen to standardlize commit msg, let bootstrap can be hoist

--- packages/simple-example/__tests__/simple-example.test.ts
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-import test from 'ava';
-
-test('1 + 1 === 2', async (t) => {
-    t.is(1 + 1, 2);
-});
--- packages/simple-example/lib/simple-example.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = simpleExample;
-
-function simpleExample() {
-    // TODO
-}
